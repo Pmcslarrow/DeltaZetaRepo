faee9dfeb7bc2af5c2826490cb905a5c18780c12
Paul McSlarrow <74205136+Pmcslarrow@users.noreply.github.com>
2022-12-15 20:50:59 -0800

    Update db.js

--- delta_zeta_server/public/scripts/db.js
+++ delta_zeta_server/public/scripts/db.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
-    password: "Loly2012!",
+    password: "NO!!!",
     host: "deltazetachapterdatabase.ckucncd2fgys.us-west-2.rds.amazonaws.com",
     port: 5432,
     database: "postgres"
